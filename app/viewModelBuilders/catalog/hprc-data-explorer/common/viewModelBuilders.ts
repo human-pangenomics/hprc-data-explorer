81162fe2b74103fe62cbd51938f14c7074068b1d
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-08-04 21:42:57 -0700

    feat: add columns to raw sequencing data list (#3)

--- app/viewModelBuilders/catalog/hprc-data-explorer/common/viewModelBuilders.ts
+++ app/viewModelBuilders/catalog/hprc-data-explorer/common/viewModelBuilders.ts
@@ -2,6 +2,32 @@ import { HPRCDataExplorerRawSequencingData } from "app/apis/catalog/hprc-data-ex
 import * as C from "../../../../components/index";
 
 /**
+ * Build props for the data type cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildDataType = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.dataType,
+  };
+};
+
+/**
+ * Build props for the design description cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildDesignDescription = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.designDescription,
+  };
+};
+
+/**
  * Build props for the filename cell.
  * @param rawSequencingData - Raw sequencing data entity.
  * @returns Props to be used for the cell.
@@ -11,5 +37,187 @@ export const buildFilename = (
 ): React.ComponentProps<typeof C.BasicCell> => {
   return {
     value: rawSequencingData.filename,
+  };
+};
+
+/**
+ * Build props for the generator contact cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildGeneratorContact = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.generatorContact,
+  };
+};
+
+/**
+ * Build props for the generator facility cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildGeneratorFacility = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.generatorFacility,
+  };
+};
+
+/**
+ * Build props for the intrument model cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildInstrumentModel = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.instrumentModel,
+  };
+};
+
+/**
+ * Build props for the library ID cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildLibraryId = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.libraryId,
+  };
+};
+
+/**
+ * Build props for the library layout cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildLibraryLayout = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.libraryLayout,
+  };
+};
+
+/**
+ * Build props for the library selection cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildLibrarySelection = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.librarySelection,
+  };
+};
+
+/**
+ * Build props for the library source cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildLibrarySource = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.librarySource,
+  };
+};
+
+/**
+ * Build props for the library strategy cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildLibraryStrategy = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.libraryStrategy,
+  };
+};
+
+/**
+ * Build props for the notes cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildNotes = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.notes,
+  };
+};
+
+/**
+ * Build props for the path cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildPath = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.path,
+  };
+};
+
+/**
+ * Build props for the platform cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildPlatform = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.platform,
+  };
+};
+
+/**
+ * Build props for the sample ID cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildSampleId = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.sampleId,
+  };
+};
+
+/**
+ * Build props for the shear method cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildShearMethod = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.shearMethod,
+  };
+};
+
+/**
+ * Build props for the size selection cell.
+ * @param rawSequencingData - Raw sequencing data entity.
+ * @returns Props to be used for the cell.
+ */
+export const buildSizeSelection = (
+  rawSequencingData: HPRCDataExplorerRawSequencingData
+): React.ComponentProps<typeof C.BasicCell> => {
+  return {
+    value: rawSequencingData.sizeSelection,
   };
 };
--- site-config/hprc-data-explorer/local/index/rawSequencingDataEntityConfig.ts
+++ site-config/hprc-data-explorer/local/index/rawSequencingDataEntityConfig.ts
@@ -19,6 +19,83 @@ import {
  */
 export const rawSequencingDataEntityConfig: EntityConfig<HPRCDataExplorerRawSequencingData> =
   {
+    categoryGroupConfig: {
+      categoryGroups: [
+        {
+          categoryConfigs: [
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.DATA_TYPE,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.DATA_TYPE,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.DESIGN_DESCRIPTION,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.DESIGN_DESCRIPTION,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.FILENAME,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.FILENAME,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.GENERATOR_CONTACT,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.GENERATOR_CONTACT,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.GENERATOR_FACILITY,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.GENERATOR_FACILITY,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.INSTRUMENT_MODEL,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.INSTRUMENT_MODEL,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_ID,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_ID,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_LAYOUT,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_LAYOUT,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_SELECTION,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_SELECTION,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_SOURCE,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_SOURCE,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_STRATEGY,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_STRATEGY,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.NOTES,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.NOTES,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.PATH,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.PATH,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.PLATFORM,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.PLATFORM,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.SAMPLE_ID,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SAMPLE_ID,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.SHEAR_METHOD,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SHEAR_METHOD,
+            },
+            {
+              key: HPRC_DATA_EXPLORER_CATEGORY_KEY.SIZE_SELECTION,
+              label: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SIZE_SELECTION,
+            },
+          ],
+        },
+      ],
+      key: "raw-sequencing-data",
+    },
     detail: {
       detailOverviews: [],
       staticLoad: false,
@@ -40,7 +117,208 @@ export const rawSequencingDataEntityConfig: EntityConfig<HPRCDataExplorerRawSequ
           >,
           header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.FILENAME,
           id: HPRC_DATA_EXPLORER_CATEGORY_KEY.FILENAME,
+          width: { max: "1.5fr", min: "212px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildDataType,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.DATA_TYPE,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.DATA_TYPE,
+          width: { max: "0.5fr", min: "112px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildDesignDescription,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.DESIGN_DESCRIPTION,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.DESIGN_DESCRIPTION,
           width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildGeneratorContact,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.GENERATOR_CONTACT,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.GENERATOR_CONTACT,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildGeneratorFacility,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.GENERATOR_FACILITY,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.GENERATOR_FACILITY,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildInstrumentModel,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.INSTRUMENT_MODEL,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.INSTRUMENT_MODEL,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildLibraryId,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_ID,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_ID,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildLibraryLayout,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_LAYOUT,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_LAYOUT,
+          width: { max: "0.5fr", min: "112px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildLibrarySelection,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_SELECTION,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_SELECTION,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildLibrarySource,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_SOURCE,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_SOURCE,
+          width: { max: "0.5fr", min: "112px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildLibraryStrategy,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.LIBRARY_STRATEGY,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.LIBRARY_STRATEGY,
+          width: { max: "0.5fr", min: "112px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildNotes,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.NOTES,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.NOTES,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildPath,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.PATH,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.PATH,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          columnVisible: false,
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildPlatform,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.PLATFORM,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.PLATFORM,
+          width: { max: "1fr", min: "160px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildSampleId,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SAMPLE_ID,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.SAMPLE_ID,
+          width: { max: "0.5fr", min: "112px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildShearMethod,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SHEAR_METHOD,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.SHEAR_METHOD,
+          width: { max: "0.5fr", min: "160px" },
+        },
+        {
+          componentConfig: {
+            component: C.BasicCell,
+            viewBuilder: V.buildSizeSelection,
+          } as ComponentConfig<
+            typeof C.BasicCell,
+            HPRCDataExplorerRawSequencingData
+          >,
+          header: HPRC_DATA_EXPLORER_CATEGORY_LABEL.SIZE_SELECTION,
+          id: HPRC_DATA_EXPLORER_CATEGORY_KEY.SIZE_SELECTION,
+          width: { max: "0.5fr", min: "112px" },
         },
       ],
       defaultSort: {
