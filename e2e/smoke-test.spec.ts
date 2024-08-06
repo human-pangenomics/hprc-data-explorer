15bb0cef15c6fde663169ee9dbe59aec9c279c2f
Jonah Paten <jpaten@ucla.edu>
2024-08-05 23:17:24 -0700

    chore: undo test failure check (#8)

--- e2e/smoke-test.spec.ts
+++ e2e/smoke-test.spec.ts
@@ -1,10 +1,8 @@
 import { expect, test } from "@playwright/test";
 
-const REDIRECT_DESTINATION_URL = "/raw-sequencing-data-lalalala";
+const REDIRECT_DESTINATION_URL = "/raw-sequencing-data";
 
-test("Expect visiting / to redirect to /raw-sequence-data", async ({
-  page,
-}) => {
+test("Expect visiting / to redirect to the correct url", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveURL(REDIRECT_DESTINATION_URL);
 });
