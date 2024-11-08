9b0b0a83aa48e1c2b70cf3e2e95015d9bd7ae524
Fran McDade <frano-m@users.noreply.github.com>
2024-11-08 16:10:49 +1000

    chore: update findable-ui to v14.0.0 (#62) (#63)
    * chore: update findable-ui to v14.0.0 (#62)
    
    * fix: update `start` script to use `serve` (#62)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>
    Co-authored-by: hunterckx <118154470+hunterckx@users.noreply.github.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* TODO self host fonts */}
-          {/* TODO only load fonts required for the app being built */}
-          <link rel="stylesheet" href="https://use.typekit.net/qhb0geh.css" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Roboto+Mono&display=swap"
             rel="stylesheet"
