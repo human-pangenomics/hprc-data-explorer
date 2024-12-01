1f403b12ad7d15f11e4ea73e24d64c0a94cdc049
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-11-30 16:13:25 -0800

    refactor: adjust build-sequencing-data and buildHelp (#92)

--- catalog-build/build-sequencing-data.py
+++ catalog-build/build-sequencing-data.py
@@ -1,9 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-import requests
-import sys
-from buildHelp import downloadFile
+from buildHelp import download_file, get_file_sizes_from_uris
 
 # Determine the base directory of the script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -23,37 +21,8 @@ def downloadSourceFiles(urls, outputFolderPath):
     paths = []
     for url in urls:
         # Get the filename and the path where the output will be saved
-        paths.append(downloadFile(url, outputFolderPath))
+        paths.append(download_file(url, outputFolderPath))
     return paths
-
-
-def get_file_size(url, total_files, current_index):
-    """
-    Convert S3 URL to HTTPS if necessary and fetch file size using HEAD request.
-    Show progress by printing remaining files to process.
-    """
-    try:
-        # Convert S3 to HTTPS URL
-        if url.startswith("s3://"):
-            bucket_and_key = url[5:]  # Remove 's3://'
-            url = (
-                f"https://{bucket_and_key.split('/')[0]}.s3.amazonaws.com/"
-                + "/".join(bucket_and_key.split("/")[1:])
-            )
-
-        # Make HEAD request to get file size
-        response = requests.head(url, timeout=10)
-        if response.status_code == 200 and "Content-Length" in response.headers:
-            return int(response.headers["Content-Length"])
-        else:
-            return "N/A"
-    except Exception:
-        return "N/A"
-    finally:
-        # Update progress
-        remaining_files = total_files - current_index - 1
-        sys.stdout.write(f"\rRemaining files to process: {remaining_files}")
-        sys.stdout.flush()
 
 
 def joinSamples(metadataPaths, biosamplesTablePath):
@@ -87,17 +56,9 @@ def joinSamples(metadataPaths, biosamplesTablePath):
         )
     )
 
-    # Add 'file_size' column by calculating size for 'path' column
-    if "path" in joined.columns:
-        total_files = len(joined["path"])
-        joined["file_size"] = [
-            get_file_size(url, total_files, index)
-            for index, url in enumerate(joined["path"])
-        ]
-    else:
-        joined["file_size"] = "N/A"
+    joined_with_size = joined.assign(file_size=get_file_sizes_from_uris(joined["path"], "sequencing data"))
 
-    return joined
+    return joined_with_size
 
 
 if __name__ == "__main__":
@@ -107,4 +68,4 @@ if __name__ == "__main__":
     )[0]
     joined = joinSamples(metadataFiles, biosamplesTableFile)
     joined.to_csv(OUTPUT_PATH, index=False)
-    print("\nProcessing complete!")
+    print("\nSequencing data processing complete!")
--- catalog-build/buildHelp.py
+++ catalog-build/buildHelp.py
@@ -2,13 +2,51 @@ from pathlib import Path
 from urllib.parse import urlparse
 import requests
 
-def downloadFile(url, outputFolderPath):
+def download_file(url, output_folder_path):
     filename = Path(urlparse(url).path).name
-    outputPath = Path(outputFolderPath, filename)
+    output_path = Path(output_folder_path, filename)
     with requests.get(url) as r:
         if r.status_code != 200:
             raise RuntimeError(f"{url} caused error {r.status_code}. See details below:\n {r.text}")
-        print(f"Downloading:\n {url}\n to {outputPath}")
-        with open(outputPath.resolve(), "w") as f:
+        print(f"Downloading:\n {url}\n to {output_path}")
+        with open(output_path.resolve(), "w") as f:
             f.write(r.text)
-    return outputPath
+    return output_path
+
+
+def get_file_size(uri, total_files, current_index, entity_type_name):
+    """
+    Convert S3 URI to HTTPS if necessary and fetch file size using HEAD request.
+    Show progress by printing remaining files to process.
+    """
+    try:
+        # Convert S3 to HTTPS URL
+        if uri.startswith("s3://"):
+            bucket, *key_parts = uri[5:].split("/")  # Remove `s3://` and split to extract bucket and key
+            uri = f"https://{bucket}.s3.amazonaws.com/{"/".join(key_parts)}"
+
+        # Make HEAD request to get file size
+        response = requests.head(uri, timeout=10)
+        if response.status_code == 200:
+            if "Content-Length" in response.headers:
+                return int(response.headers["Content-Length"])
+            else:
+                print(f"No `Content-Length` header received from {uri}")
+                return "N/A"
+        else:
+            print(f"Received {response.status_code} response from {uri}")
+            return "N/A"
+    except Exception:
+        print(f"Exception occurred while requesting {uri}")
+        return "N/A"
+    finally:
+        # Update progress
+        remaining_files = total_files - current_index - 1
+        print(f"Remaining {entity_type_name} files to process: {remaining_files}")
+
+def get_file_sizes_from_uris(uris, entity_type_name):
+    total_files = len(uris)
+    return [
+        get_file_size(uri, total_files, index, entity_type_name)
+        for index, uri in enumerate(uris)
+    ]
