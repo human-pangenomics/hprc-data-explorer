55ef7c81288b2a0cec386f3ac09a23bf9e659be8
jpaten <jonah@clevercanary.com>
2024-12-06 14:47:56 -0800

    chore: updated lint plugins, fixed sonar issues (#96)

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,7 @@ const config: PlaywrightTestConfig = {
     },
   ],
   testDir: "e2e",
+  // eslint-disable-next-line sonarjs/slow-regex -- input is just local filenames so trusted
   testMatch: /.*\.spec\.ts/,
   timeout: 1.5 * 60 * 1000,
   use: {
