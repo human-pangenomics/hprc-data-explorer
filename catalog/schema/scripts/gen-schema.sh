b0db0306d3049ace1e39e9ce63ede54c368981fb
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-05-02 20:29:58 -0700

    refactor: derive sequencing data and assembly source key types from schemas (#136)

--- catalog/schema/scripts/gen-schema.sh
+++ catalog/schema/scripts/gen-schema.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+SCHEMA_NAMES=(sequencing_data assemblies)
+
 # Generate Pydantic models
-gen-pydantic ./catalog/schema/sequencing_data.yaml > ./catalog/build/py/generated_schema/sequencing_data.py
-gen-pydantic ./catalog/schema/assemblies.yaml > ./catalog/build/py/generated_schema/assemblies.py
+for name in ${SCHEMA_NAMES[@]}
+do
+  gen-pydantic ./catalog/schema/$name.yaml > ./catalog/build/py/generated_schema/$name.py
+done
+
+# Generate TypeScript definitions
+schema_paths=()
+for name in ${SCHEMA_NAMES[@]}
+do
+  schema_paths+=("./catalog/schema/$name.yaml")
+done
+python ./catalog/schema/scripts/gen_slot_name_types.py "${schema_paths[@]}" > ./catalog/schema/generated/schema.ts
