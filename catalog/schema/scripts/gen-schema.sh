349ff9cd99005785bac984f48f2767c767b1571c
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2025-05-07 12:58:11 -0700

    feat: allow extra fields when validating catalog source (#155) (#158)

--- catalog/schema/scripts/gen-schema.sh
+++ catalog/schema/scripts/gen-schema.sh
@@ -5,7 +5,7 @@ SCHEMA_NAMES=(sequencing_data assemblies annotations alignments)
 # Generate Pydantic models
 for name in ${SCHEMA_NAMES[@]}
 do
-  gen-pydantic ./catalog/schema/$name.yaml > ./catalog/build/py/generated_schema/$name.py
+  gen-pydantic --extra-fields ignore ./catalog/schema/$name.yaml > ./catalog/build/py/generated_schema/$name.py
 done
 
 # Generate TypeScript definitions
