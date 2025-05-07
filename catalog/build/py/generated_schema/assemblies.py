c8859892029b21a6f727d184305c2009f4ea9c1b
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-05-07 12:39:03 -0700

    feat: allow extra fields when validating catalog source (#155)

--- catalog/build/py/generated_schema/assemblies.py
+++ catalog/build/py/generated_schema/assemblies.py
@@ -36,7 +36,7 @@ class ConfiguredBaseModel(BaseModel):
     model_config = ConfigDict(
         validate_assignment = True,
         validate_default = True,
-        extra = "forbid",
+        extra = "ignore",
         arbitrary_types_allowed = True,
         use_enum_values = True,
         strict = False,
